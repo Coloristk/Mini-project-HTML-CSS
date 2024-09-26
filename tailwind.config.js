471d93c30bb5358b5cf27f0f13877306c8f16842
Kong Thiraphat <thiraphat.cto@gmail.com>
2024-09-26 15:38:06 +0700

    feat: add cta section and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
         'white': '#ffffff',
         'gray' : '#C4C4C4',
         'yellow' : '#FFA502'
-      }
-    }
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        rubik: ['Rubik', 'sans-serif'],
+      },
+    },
+    plugins: [],
   }
