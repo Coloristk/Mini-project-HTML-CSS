4b14a6a8e0944859d88ed1193b5321e58c48de3d
Kong Thiraphat <thiraphat.cto@gmail.com>
2024-09-26 09:42:22 +0700

    set up mini project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      colors: {
+        'white': '#ffffff',
+        'gray' : '#C4C4C4',
+        'yellow' : '#FFA502'
+      }
+    }
+  }
